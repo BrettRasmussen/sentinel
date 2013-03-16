0e6200e1de4c47542105cc6141c916270fa98192
Sentinel <brett.rasmussen@twoedge.com>
2013-03-16 15:05:20 -0600

    change system calls to use bin scripts; rename 'filter' to 'firewall'

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,29 +9,31 @@ class MainController < ApplicationController
 
   def unlock
     begin
-      timeout(3) { %x{sudo gpasswd -a brett admin} }
+      unlock_cmd = Rails.root.join('bin', 'unlock')
+      timeout(3) { %x{sudo #{unlock_cmd}} }
       groups = []
       timeout(3) { groups = %x{sudo groups brett}.chomp.match(/^.*:(.*)/).to_a[1].split }
       if groups.include?('admin')
-        flash[:notice] = 'Filter has been unlocked.'
+        flash[:notice] = 'Firewall has been unlocked.'
       else
         raise 'Failed to grant admin rights.'
       end
     rescue => e
-      flash[:error] = "Could not unlock filter: #{e.message}"
+      flash[:error] = "Could not unlock firewall: #{e.message}"
     end
     redirect_to :action => :index
   end
 
   def lock
     begin
-      timeout(3) { %x{sudo gpasswd -d brett admin} }
+      lock_cmd = Rails.root.join('bin', 'lock')
+      timeout(3) { %x{sudo #{lock_cmd}} }
       groups = ['admin']
       timeout(3) { groups = %x{sudo groups brett}.chomp.match(/^.*:(.*)/).to_a[1].split }
       raise 'Failed to revoke admin rights.' if groups.include?('admin')
-      flash[:notice] = 'Filter has been locked.'
+      flash[:notice] = 'Firewall has been locked.'
     rescue => e
-      flash[:error] = "Could not lock filter: #{e.message}"
+      flash[:error] = "Could not lock firewall: #{e.message}"
     end
     redirect_to :action => :index
   end

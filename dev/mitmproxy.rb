0bc4d18550e9746cddabc0029338467ab4d248c8
Brett Rasmussen <brett.rasmussen@twoedge.com>
2013-05-12 20:17:16 -0600

    working whitelist functionality (so far, images only), beginnings of mitmproxy

---
+++ dev/mitmproxy.rb
@@ -0,0 +1,29 @@
+require 'webrick'
+require 'webrick/ssl'
+require 'webrick/httpproxy'
+
+class MitmProxyServer < WEBrick::HTTPProxyServer
+  #def service(req, res)
+    #if req.request_method == "CONNECT"
+      #do_CONNECT(req, res)
+    #elsif req.unparsed_uri =~ %r!^http://!
+      #proxy_service(req, res)
+    #else
+      #super(req, res)
+    #end
+  #end
+
+  def service(req, res)
+    if req.request_method == "CONNECT"
+      debugger
+      @config[:SSLEnable] = true
+      @ssl_context = setup_ssl_context({
+        :SSLCertName => [['CN', 'twoedge.com']],
+        :SSLCertComment => ""
+      })
+      super(req, res)
+    else
+      super(req, res)
+    end
+  end
+end
